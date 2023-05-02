f2bdc6e749ab72251208f46b51c3463be40c844c
Cheoljun Park <dizncjfwns@nate.com>
2023-05-02 21:20:54 +0900

    Add files via upload

---
+++ fps/src/main/java/capstone/onye/fps/FpsApplication.java
@@ -0,0 +1,13 @@
+package capstone.onye.fps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpsApplication.class, args);
+	}
+
+}
